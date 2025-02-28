c1b726a27f09054d9599f5b54bafa821f482955a
Emediong <emediongcharlie@gmail.com>
2025-02-28 15:15:12 +0100

    parameterized

--- src/main/java/com/demo/docker/controllers/DockerController.java
+++ src/main/java/com/demo/docker/controllers/DockerController.java
@@ -5,10 +5,7 @@ import com.demo.docker.dtos.response.LoginResponse;
 import com.demo.docker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/")
@@ -23,7 +20,7 @@ public class DockerController {
     }
 
     @PostMapping("login")
-    public ResponseEntity<?> USerLogin(LoginRequest loginRequest) {
+    public ResponseEntity<?> USerLogin(@RequestBody LoginRequest loginRequest) {
         try{
             LoginResponse response = userService.login(loginRequest);
             return ResponseEntity.ok(response);
